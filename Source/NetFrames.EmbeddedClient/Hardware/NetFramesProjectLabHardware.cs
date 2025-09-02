10d3e715da6cab8386095dece24dbdccc486e296
Jorge Ramirez <ramirez.jorgea@outlook.com>
2025-09-01 22:16:30 -0700

    Update to 2.4.0.4 beta nugets

--- Source/NetFrames.EmbeddedClient/Hardware/NetFramesProjectLabHardware.cs
+++ Source/NetFrames.EmbeddedClient/Hardware/NetFramesProjectLabHardware.cs
@@ -1,25 +1,19 @@
-﻿using Meadow.Devices;
-using Meadow.Hardware;
-using Meadow.Peripherals.Displays;
-using Meadow.Peripherals.Leds;
-using NetFrames.EmbeddedClient.Contracts;
+﻿namespace NetFrames.EmbeddedClient.Hardware;
 
-namespace NetFrames.EmbeddedClient.Hardware;
+//public class NetFramesProjectLabHardware : INetFramesHardware
+//{
+//    private readonly IProjectLabHardware projectLab;
 
-public class NetFramesProjectLabHardware : INetFramesHardware
-{
-    private readonly IProjectLabHardware projectLab;
+//    public IRgbPwmLed? RgbPwmLed => projectLab.RgbLed;
 
-    public IRgbPwmLed? RgbPwmLed => projectLab.RgbLed;
+//    public IPixelDisplay? Display => (IRotatableDisplay?)projectLab.Display;
 
-    public IPixelDisplay? Display => (IRotatableDisplay?)projectLab.Display;
+//    public RotationType DisplayRotation => RotationType._270Degrees;
 
-    public RotationType DisplayRotation => RotationType._270Degrees;
+//    public IWiFiNetworkAdapter NetworkAdapter => projectLab.ComputeModule.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
 
-    public IWiFiNetworkAdapter NetworkAdapter => projectLab.ComputeModule.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
-
-    public NetFramesProjectLabHardware(IProjectLabHardware projectLab)
-    {
-        this.projectLab = projectLab;
-    }
-}
+//    public NetFramesProjectLabHardware(IProjectLabHardware projectLab)
+//    {
+//        this.projectLab = projectLab;
+//    }
+//}
