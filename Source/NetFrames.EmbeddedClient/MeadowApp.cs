0fa154238c41fe8e0d88cec57bfcc53310c0befd
Jorge Ramirez <ramirez.jorgea@outlook.com>
2025-09-23 11:26:39 -0700

    Bump to 2.1 (OTA test)

--- Source/NetFrames.EmbeddedClient/MeadowApp.cs
+++ Source/NetFrames.EmbeddedClient/MeadowApp.cs
@@ -29,7 +29,7 @@ OTA instructions:
 //public class MeadowApp : ProjectLabCoreComputeApp
 public class MeadowApp : App<F7FeatherV2>
 {
-    public static double VERSION { get; set; } = 2.1;
+    public static double VERSION { get; set; } = 2.2;
 
     private MainController? mainController;
 
